e9c0483e5a0ed39e58be6b6428f19b2929eb6cd6
JARS <J.ARS96@outlook.com>
2022-09-05 23:04:35 -0500

    env & cors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ const taskRoutes = require('./routes/tasks.routes');
 
 const app = express();
 app.use(morgan('dev'));
+app.use(express.json())
 app.use(taskRoutes);
+
+app.use((err, req, res, next) => {
+    return res.json({
+        message: err.message
+    })
+});
 
 
 const serverPORT = 3000;
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,24 +1,17 @@
-const {Router} = require('express');
+const { Router } = require('express');
+const { getAlltasks, getSingleTask, createTask, deleteTask, updateTask } = require('../controllers/tasks.controllers');
+//const pool = require('../db');
+
 const router = Router();
 
-router.get('/tasks', (req, res) => {
-    res.send('retrieving a list of tasks');
-});
+router.get('/tasks', getAlltasks);
 
-router.get('/tasks/:id', (req, res) => {
-    res.send('retrieving a list of tasks');
-});
+router.get('/tasks/:id', getSingleTask);
 
-router.post('/tasks', (req, res) => {
-    res.send('creating a task');
-});
+router.post('/tasks', createTask);
 
-router.delete('/tasks', (req, res) => {
-    res.send('deleting a task');
-});
+router.delete('/tasks/:id', deleteTask);
 
-router.put('/tasks', (req, res) => {
-    res.send('updating task');
-});
+router.put('/tasks/:id', updateTask);
 
 module.exports = router;
